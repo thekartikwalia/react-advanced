28c17f61fb4dfdf5948afd48b38e24f95109bc41
Kartik-Walia <itzkartikeds3@gmail.com>
2025-01-30 00:59:43 +0530

    Removed unnecessary re-renders of Counter component (Learned memo)

--- src/App.js
+++ src/App.js
@@ -6,20 +6,33 @@ import CounterButton from "./components/CounterButton";
 function App() {
   console.log("APP RENDERED");
   const [count, setCount] = useState(0);
+  const [enteredCount, setEnteredCount] = useState(0);
 
   const handleIncrement = () => {
     setCount((prev) => prev + 1);
-  }
+  };
 
   const handleDecrement = () => {
     setCount((prev) => prev - 1);
-  }
+  };
+
+  const handleEnteredCountChange = (event) => {
+    setEnteredCount(event.target.value);
+  };
 
   return (
     <div>
       <Counter count={count} />
       <CounterButton onButtonClick={handleIncrement}>Increase</CounterButton>
       <CounterButton onButtonClick={handleDecrement}>Decrease</CounterButton>
+      <div>
+        <input
+          type="number"
+          value={enteredCount}
+          onChange={handleEnteredCountChange}
+        />
+        <button onClick={() => setCount(enteredCount)}>Set</button>
+      </div>
     </div>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,23 @@
-function Counter ({ count }) {
+import { memo } from "react";
+
+// Way-1 of Optimisation (using "memo" to avoid unnecessary re-renders)
+
+// memo ne check kiya ki kya iss component ke props change hue hain
+// Agar change hue hain props, toh re-render kardo
+// Ptherwise, re-render mat karo (faltu re-render)
+// This is a way of Optimisation
+
+// But don't overuse memo
+// Check of old prop and new prop is also an extra computation 
+// So it's overuse would degrade the performance 
+
+// You should use "memo" in case of top level big components
+// whose props aren't changing, but they're getting re-rendered 
+// unnecessarily due to re-rendering of their parent
+
+const Counter = memo(function Counter ({ count }) {
     console.log("COUNTER RENDERED");
     return <h1>Counter: {count}</h1>;
-}
+});
 
 export default Counter;
