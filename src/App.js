d4450b75451b8280c8108449bb209f12981df2f0
Kartik-Walia <itzkartikeds3@gmail.com>
2025-01-31 02:10:14 +0530

    Form Validation using useEffect hook (Learned useEffect, places where hooks aren't necessary)

--- src/App.js
+++ src/App.js
@@ -2,47 +2,18 @@ import { useEffect, useState } from "react";
 import Login from "./componentsForUseEffect/Login";
 import Home from "./componentsForUseEffect/Home";
 
-// We can now easily manipulate DOM using state, context, and props
-// Today, we're gonna talk about Side-Effects (or Effects)
-// Yeh voh piece of code hai jo component render cycle mei nahi aana chahta
-// But component ke ander rehna chahta hai (kyuki usko props, states chahiye hongi)
+import "./App.css";
 
-// Eg-1 : Browser ki apni local storage mei tumhe kuch store karna hai
-// Agar voh component render cycle ka part hota, toh toh fir baar baar save hota rehta
-// Voh hum nahi chahte, offcourse performance ki dikkat ayegi na fir
-
-// Eg-2 : HTTP request bhejni ho backend server pe
-// Hum lagatar toh bhej nahi sakte, yeh code render cycle mei toh nahi rakhna
-// par component ke ander rakhna hai
-
-// Uske liye hote hein Side Effects, aur isko implement karne ke liye react ek hook provide karta hai useEffect
-
-// Side-Effects ka kaam kya hota hai, voh piece of code rakhna joki hum render cycle mei nhi rakhna chahte,
-// apne hisaab se execute karna chahte hai (ki yeh yeh change ho, tabhi execute karna)
-
-// On refreshing i loose my Login state, aisa kyu hora hai ?
-// Maine login kiya, toh isLoggedIn state hogyi true
-// Aur agar mai refresh karta hun, toh mera app wapas scratch se load hora hai, toh default state mei chla gya
-
-// Merko kuch aisa karna hai ki meri app MAINTAIN rahe, mtlb iska LOGIN status maintained rahe
-// Voh mai kaise kar sakta hun ?
-// Voh mai aise kar sakta hun ki abhi ke liye kahi aur daalna pdega merko iss state ko
-// Iss code mei toh kahi rakh nhi sakta, iss code mei kahin bhi rakhunga app refresh hote he meri state loose hojayegi
-// Isliya mai isko kahan rakhdunga, local storage mei iski state rakhni pdegi mujhe
-
-// Mai kya chahta hun ki yeh login ho, toh isko jo isLoggedIn state hai, voh localStorage mei chli jaye
-// Aur jab app refresh ho toh mai usko wahan se wapis retrieve karlu
-// Agar voh loggedIn true hai, toh wapis seedha isLoggedIn true chla jaye uske paas, yeh mai chahta hun
+// Main chahta hun ki email ki length and password ki length greater than 5 ho, 
+// tohi mai login dbaunga, nahi toh mera button disabled rahe 
 
 function App() {
-  console.log("APP RENDERING");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     const storedLoginStatus = localStorage.getItem("isLoggedIn");
 
     if (storedLoginStatus === "1") {
-      // matlab ki user LoggedIn hai, toh seedha isLoggedIn ko kardo true, fir hmara Home render hojayega
       setIsLoggedIn(true);
     }
   }, []);
@@ -53,54 +24,11 @@ function App() {
     localStorage.setItem("isLoggedIn", "1");
   };
 
-  // Ismei bhot bdi galti hai, ki jaise he mera App chla usne toh check krliya ki localStorage mei status hai ki nahi h
-  // Agar status 1 hua toh yeh state wapas set kardega 1
-  // State wapas set kardega matlab component re-render hoga
-  // Re-render hoga with the new value of isLoggedIn
-  // Fir yeh wapas localStorage se status nikla
-  // Toh yeh ek INFINITE loop banjara hai, upar waale code ke kaaran
-  // Above explanation is in case of absence of useEffect 
-
-  // Ab isko theek karne ka jo treka hota hai, vohi hota hai SIDE-EFFECTS
-  // Side-Effect ka ek hook hai useEffect side effect daalne ke liye
-  // 2 parameters hote hain : function and array of dependencies
-
-  // Function mei merko vohi piece of code daalna hai, joki mai chahta hun as Side-Efect
-
-  // Yeh kaise kaam karta hai dekhna 
-  // Pehle mera App render hoga, useState false hui
-  // Jab yeh ek baar render hojata hai (sabkuch except useEffect), uske baad yeh aata hai useEffect par,
-  // Aur dekhta hai ki kya iski dependencies change hui hain
-  // Aur pehli baar jab yeh chlega, toh 1st time ke liye yeh maanta hai ki haan hui hai 
-  // Toh 1st time toh yeh useEffect ek baar toh humesha execute hota hai App ke chalte he 
-
-  // Sabse pehle App render hora hai
-  // useEffect wala block execute nhi hua 
-  // Sab render hua, aur fir wapas useEffect ke block pe aaya 
-  // 1st render ke baad useEffect pe aajata hai and check karta hai ki yeh dependency mei chnage hai ya nahi 
-  // 1st time change hai he, toh yeh code run hojayega, toh 1st time chalte he yeh state true hojayegi
-  // Toh wapas re-render hoke state true kardega 
-  // Iss tarah se mera Login status True set hojayega jab yeh chlega 
-
-  // Array of dependencies jab tum khaali dete ho, 
-  // Iska matlab hai ki mera App sirf tabhi chlega jab 1st time mera App render hua 
-  // Waise dependencies mei tumhe state, functions, variables used dena hota hai
-  // Ki bhyii jab jab meri dependencies change ho iss code ki, toh he yeh code wapis run karna 
-  // Agar tum khaali dete ho iska mtlb ki voh 1st time chalega 
-
-  // APP RENDERING 2 baar iss kaaran hora hai kyuki yahan pe 
-  // useEffect mei state ek aur baar set hoja rahi hai 
-
-  // localStorage ko zarurat nahi hai as dependency daalne ki
-
   const handleLogout = () => {
     setIsLoggedIn(false);
 
     localStorage.removeItem("isLoggedIn");
   };
-
-  // We maintained login and logout status, based on usne pehle kya kiya tha 
-  // LocalStorage ki help se 
 
   return (
     <div>
--- src/componentsForUseEffect/Login.js
+++ src/componentsForUseEffect/Login.js
@@ -1,12 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Login = ({ onLogin }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  // const [isFormValid, setIsFormValid] = useState(false);
+  // If form gets valid (lengths > 5), then enable the Login button
+
+  // Mai karta hun ab Side-Effects introduce
+  // Ek application merko side-effect ka yeh dikha tha ki isFormValid state jo hai
+  // voh 2 states pe depend kar raha hai, and isko maine 2 onChnage handlers mei daal rakha hai
+  // Instead of this, mai isko as a side-effect daal sakta hun ki jab jab meri yeh dono states (email & password)
+  // change ho, tab isFormValid ko calculate karlo
+
+  // useEffect(() => {
+  //   setIsFormValid(email.length > 5 && password.length > 5);
+  // }, [email, password]);
+  // Jab jab emaila ur password change hon tab tab yeh automatically run hojaye apne aap
+
+  // But kya merko isFormValid state banane ki zarurat thi ?
+  // Yeh depend toh sirf email aur password ki length pe kar raha hai
+  // Mai isko even state se bhi hata sakta hun kyuki yeh sirf email aur password state pe depend kar raha hai
+  // Jab jab emaila ur password change honge, tab mera ye component waise he re-render hoga
+  // Aur waise he re-render hoga toh mai isko as a variable bhi rakh sakta hun yahan pe
+  // Since yeh state pe he depend kar raha hai toh merko isko another state bnane ki zrurat he nahi hai
+
+  const isFormValid = email.length > 5 && password.length > 5;
+  // shi chlra kyuki email password hone pe waise he re-render hojayega component
+  // re-render hoga toh as a variable he daal lo
+
+  // Even side-effect mei nahi chahiye mujhe yahan pe
+  // Toh hook kabhi kabhi use nahi bhi karna hota
+
   const handleSubmit = (event) => {
     event.preventDefault();
     onLogin();
+  };
+
+  const handleEmailChange = (e) => {
+    const enteredEmail = e.target.value;
+    setEmail(enteredEmail);
+    // setIsFormValid(enteredEmail.length > 5 && password.length > 5);
+  };
+
+  const handlePasswordChange = (e) => {
+    const enteredPassword = e.target.value;
+    setPassword(enteredPassword);
+    // setIsFormValid(enteredPassword.length > 5 && email.length > 5);
   };
 
   return (
@@ -14,18 +54,20 @@ const Login = ({ onLogin }) => {
       <input
         type="text"
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={handleEmailChange}
         placeholder="Email"
         required
       />
       <input
         type="password"
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={handlePasswordChange}
         placeholder="Password"
         required
       />
-      <button type="submit">Login</button>
+      <button type="submit" className={`${isFormValid ? "" : "disabled"}`}>
+        Login
+      </button>
     </form>
   );
 };
