86ea6c7972a9a85728eb5ba2858cb58c28766af0
Kartik-Walia <itzkartikeds3@gmail.com>
2025-01-31 12:57:47 +0530

    Learned Client-Server Architecture, API, and types of HTTP requests

--- src/App.js
+++ src/App.js
@@ -4,6 +4,53 @@ import Home from "./componentsForUseEffect/Home";
 
 import "./App.css";
 
+// We don't store data in Frontend to keep data protected 
+// We store data in Database (collection of data)
+// Aur voh data kis form mei rakha jayega, uss basis pe bhot saare types of databases hote hain 
+// Like: MongoDB, mySQL, PostgresSQL
+
+// React App and Database kabhi bhi directly bat cheet nahi karte 
+// Kyuki database se connect karne ke liye usse chahiye database ke credentials 
+// And fir merko credentials frontend mei rakhne padenge, which is security issue 
+
+// Isliye tumhare paas hota hai ek Backend server 
+// Backend server ka kaam hota hai ki database se jab data ko fetch karna hai 
+// Jab client ko chahiye data, toh data usse nikal ke dedo 
+// Authentication ka kaam karta hai backend server (Middlewares n all are also held in Backend server)
+// You can write this in any language (PHP, NodeJS, Django, Golang, Rust)
+
+// Kaam same he hai ki jab request aaye frontend se, toh data bhejdo ya data mei kuch add karna hai toh voh krlo
+// Voh saara logic likha hota hai backend server pe
+
+// Toh react baatcheet karega backend server se, aur kahega ki bhyii yeh wala data merko chahiye 
+
+// Yeh toh hogya CLIENT-SERVER MODEL
+// CLient-Server model kya hai ? Ki merpe ek hai Client (browser, mobile app, desktop app, etc.)
+// Aur ek ho sakta hai Server, aur server interact karta hai Database se 
+// Iss tarah se yeh teeno interact kar rahe hote hain 
+
+// CLient ko data chahiye hoga, toh voh Server pe ek request maarega (HTTP request)
+// HTTP ek protocol (set of rules) hai
+// Jab bhi mai koi request maarta hun Client se Server pe, 
+// toh yeh set of rules follow karne waali ko request hoti hai, usko HTTP request kehte hain 
+
+// There are 4 types of HTTP request :
+// 1. GET -> Mai kuch data maangra hun database se, ki yeh data send kardo
+// 2. POST -> Yeh data submit kardo, yeh data mera database mei add hojaye (like form submit sends a post request)
+// 3. PUT -> update kardo mera data jo already database mei hai (like profile updation)
+// 4. DELETE -> mujhe kuch delete karna hai apne database mei se 
+// All 4 of these follow set of rules defined by HTTP protocol
+
+// Now, let's talk about API (Appplication Programming Interface) 
+// API is like restaurant ka Menu 
+// Ek server ke paas kuch requests ko handle karne ka logic likha hua hai 
+// Uske liye usne API define kiya hua hai, API hai uss server ka menu card
+// Koi bhi request aayi kisi endpoint pe toh yeh kardena 
+// Aur requests ke liye define hote hai API endpoints 
+// HTTP request jo jaati hai, voh backend server ke API endpoint pe aati hai 
+// Aur fir API endpoint ke baad server handle karta hai usko
+// bass yehi tha hmara API, API kuch nhi bss server ka menu card hai
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
--- src/componentsForUseEffect/Login.js
+++ src/componentsForUseEffect/Login.js
@@ -1,26 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-// Let's now understand CLEANUP Function
-
-// Jaise he mai type kar raha hun, mere har keystroke pe "checking is form valid" chal ra hai
-// Mai chahta hun ki har key stroke pe na chle, jab user type kar raha hai toh usko type karne de
-// Jab uski typing ruk jaye, tab form valid check kare
-
-// Isko bolte hain DE-BOUNCING Effect (isko laane ke liye use hota hai useEffect hook)
-// ki mai itna fast fast apne state ko call nhi karna chahta
-// Mai chahta hun ki vo kuch break le 500ms ka aur fir yeh chale
-
-// Agar maine kuch multiple letters 500ms ke timeout ke ander likhe hain
-// Toh voh clear kardo 
-// Mai pichle wala timeout clear karna chahta hun 
-// Uske liye useEffect mei hota hai CLEANUP function
-
-// Har key stroke pe pichle key stroke ka timer clear karna pdega
-// CLEANUP function kya karta hai, har useEffect ke chalne se pehle
-// Pehle CLEANUP function chlta hai (Bss pehli baar cleanup nahi chlta)
-
-// Last mei sirf ek he timer bacha, jo maine last keystroke pe daala tha
-
 const Login = ({ onLogin }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -33,9 +12,6 @@ const Login = ({ onLogin }) => {
       setIsFormValid(email.trim().length > 5 && password.trim().length > 5);
     }, 500);
 
-    // CLEAN UP : Yeh aisa function hone wala hai 
-    // Jo pichla useEffect jab run hua tha, yeh us se pehle execute hojaye
-    // this gets called before execution of above piece of code (setTimeout) 
     return () => {
       console.log("CLEANUP");
       clearTimeout(timeoutId);
